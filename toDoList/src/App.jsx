4535284ba78ca08b7a397d7879e98504a2b600a3
Raman <rs359388@gmail.com>
2024-03-21 23:27:02 +0530

    first commit

---
+++ toDoList/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const toDo = (toDoList) => {
+  <div className = "background colour = #384924" >toDo
+    <span color='red' width = "40px" height = "40px" >Red</span>
+    <span color='blue' width = "40px" height = "40px">blue</span>
+    <span color='green' width = "40px" height = "40px">green</span>
+    <span color='purple' width = "40px" height = "40px">Purple</span>
+    </div>
+  return (
+    toDoList(toDo)
+  )
+}
+
+export default toDo
